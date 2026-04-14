7d4218e9ea964d9660a3f9b6819ca4d51283019b
Jyotish Pabbisetti <jyotishpabbisetti2025@gmail.com>
2026-04-14 12:59:45 +0530

    Time: 373 ms (65.63%) | Memory: 31.8 MB (62.50%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,11 +1,18 @@
 class Solution(object):
     def rangeSum(self, nums, n, left, right):
         mod = 10**9 + 7
+        
+        # Build prefix sum
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i+1] = prefix[i] + nums[i]
+
         arr = []
 
+        # Generate all subarray sums in O(1) each
         for i in range(n):
             for j in range(i, n):
-                arr.append(sum(nums[i:j+1]))  # O(n)
+                arr.append(prefix[j+1] - prefix[i])
 
         arr.sort()
 
